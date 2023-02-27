b974a50f7526c76f185fb051a01d486ca8d76055
kninich55 <tahicham5@gmail.com>
2023-02-27 17:22:57 -0500

    implement main

--- src/nih/gov/App1.java
+++ src/nih/gov/App1.java
@@ -12,33 +12,67 @@ public class App1 {
        //create 10 student
         for (int i = 0; i < 10; i++) {
             Student st=new Student();
-            System.out.println(st.toString());
+            //System.out.println(st.toString());
             studentList.add(st);
         }
         studentList=takeExam();
 
-        System.out.println("-----------After taking First Exam-----------");
+        System.out.println("-----------After taking all 3 Exam-----------");
 
         for(Student st:studentList)
             System.out.println(st.toString());
-    }
 
-    private static List<Student> takeExam() {
-        for (Student st:studentList) {
-            //take first exam
-            int[] grades = new int[3];
-            int grade1= (int) (Math.random() * 100);
-            grades[0]=grade1;
-            st.setGrades(grades);
+        System.out.println("-----------Ranking graduated student gpa>70 level advanced-----------");
 
-            if(grade1>=50)
-                st.setLevel(LEVEL.INTERMEDIATE);
+        for(Student st:studentList) {
+            if(st.getLevel().equals(LEVEL.ADVANCED) && st.getGpa()>=70) {
+                System.out.println(st.toString());
+            }
 
-            st.setGpa((int) grade1/3);
+        }
 
+        //todo
+        System.out.println("-----------Ranking all students-----------");
+
+        for(Student st:studentList) {
+
+                System.out.println(st.toString());
 
 
         }
+    }
+
+    private static List<Student> takeExam() {
+
+        for (Student st:studentList) {
+            //take first exam
+            int[] grades = new int[3];
+            int grade0= (int) (Math.random() * 100);
+            grades[0]=grade0;
+            st.setGrades(grades);
+            st.setGpa((int) grade0/3);
+
+            if(grade0>=50) {
+                st.setLevel(LEVEL.INTERMEDIATE);
+                int grade1= (int) (Math.random() * 100);
+                grades[1]=grade1;
+                st.setGrades(grades);
+                st.setGpa((int) (grade0+grade1) / 3);
+
+                if(grade1>=50){
+
+                    st.setLevel(LEVEL.ADVANCED);
+                    int grade2= (int) (Math.random() * 100);
+                    grades[2]=grade2;
+                    st.setGrades(grades);
+                    st.setGpa((int) (grade0+grade1+grade2) / 3);
+
+                }
+
+                }
+
+            }
+
         return studentList;
     }
 }
